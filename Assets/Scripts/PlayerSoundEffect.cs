76a754389778037a9561c5fd817643c02d0c597f
Alorcus <raban.giessler@student.hpi.de>
2021-06-10 08:24:39 +0200

    add PlayerSoundEffects script

---
+++ Assets/Scripts/PlayerSoundEffect.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using SpeechIO;
+public class PlayerSoundEffect : MonoBehaviour
+{
+    public AudioClip dropInClip;
+    public AudioClip gameOverClip;
+    public AudioClip collisionClip;
+    public float maxPitch = 1.2f;
+    public float minPitch = 0.8f;
+    private GameObject previousEnemy;
+    private AudioSource audioSource;
+    public SpeechOut speechOut = new SpeechOut();
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public float PlayerFellDown()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(gameOverClip);
+        return gameOverClip.length;
+    }
+    public void PlayHit()
+    {
+        PlayClipPitched(collisionClip, minPitch, maxPitch);
+    }
+    public void PlayDropIn()
+    {
+        audioSource.PlayOneShot(dropInClip);
+    }
+
+    public void StopPlayback()
+    {
+        audioSource.Stop();
+    }
+
+    public void PlayClipPitched(AudioClip clip, float minPitch, float maxPitch)
+    {
+        // little trick to make clip sound less redundant
+        audioSource.pitch = Random.Range(minPitch, maxPitch);
+        // plays same clip only once, this way no overlapping
+        audioSource.PlayOneShot(clip);
+        audioSource.pitch = 1f;
+    }
+
+}
