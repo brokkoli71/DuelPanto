a5863018c982504699197bb26a336cd33b8af350
brokkoli71 <brokkoli71b@gmail.com>
2021-07-05 21:37:21 +0200

    implementing new it behaviour

---
+++ Assets/Scripts/ExprorerScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using DualPantoFramework;
+using System;
+
+public class ExprorerScript : MonoBehaviour
+{
+    PantoHandle lowerHandle, upperHandle;
+    GameObject player;
+    bool exploring;
+    bool isPiched;
+    bool isEnabled;
+    public float activateDistance = 1;
+    void Start()
+    {
+        lowerHandle = GameObject.Find("Panto").GetComponent<LowerHandle>();
+        upperHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+        player = GameObject.Find("Player");
+        isEnabled = false;
+        exploring = false;
+    }
+
+    void Update()
+    {
+        bool isHome = Vector3.Distance(upperHandle.transform.position, lowerHandle.transform.position ) < activateDistance;
+
+        if (!isHome && !exploring) { // geht weg
+            exploring = true;
+            startingExploring();
+        }
+        if(isHome && exploring)//wieder zurueck
+        {
+            exploring = false;
+            endExproring();
+        }
+        try 
+        {
+            isPiched = GameObject.Find("Player").GetComponent<PlayerLogic>().isPitched;
+            if (isEnabled && !isPiched && isHome)//me faengt an sich zu bewegen
+            {
+                endExproring();
+            }
+            else if (!isEnabled && isPiched) //me hoert auf sich zu bewegen
+            {
+                enableExproring();
+            }
+        }
+        catch(NullReferenceException)
+        {
+            isPiched = true;
+        }
+        
+
+    }
+
+    void enableExproring()
+    {
+        lowerHandle.Free();
+    }
+    void startingExploring()
+    {
+        upperHandle.Freeze();
+    }
+    async void endExproring()
+    {
+        await lowerHandle.SwitchTo(player);
+    }
+}
